ffcc29ff47b8a84f5a181329862b7560f127b808
Wenchao Hao <wenha217@student.liu.se>
2015-11-16 22:25:14 +0100

    Search for a range problem sovled

--- src/SearchForARange.java
+++ src/SearchForARange.java
@@ -15,5 +15,35 @@
 public class SearchForARange {
 
 	//try git new branch and merge branch
-	今天心情很不好。不写了。
+	//今天心情很不好。不写了。
+	public int[] searchRange(int[] nums,int target){
+		int[] ret = {-1, -1};
+        int start = 0;
+        int end = nums.length - 1;
+        while (start < end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] < target)
+                start = mid + 1;
+            else
+                end = mid;
+        }
+        int low;
+        if (nums[start] != target)
+            return ret;
+        else
+            low = start;
+        start = 0;
+        end = nums.length - 1;
+        while (start < end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] < target + 1)
+                start = mid + 1;
+            else
+                end = mid;
+        }
+        int high = nums[start] == target ? start : start - 1;
+        ret[0] = low;
+        ret[1] = high;
+        return ret;
+	}
 }
